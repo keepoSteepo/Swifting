d45f24b7ac78f29dd71db673685489aaafa40d96
Keepo <Keepitk.xprezz@yahoo.com>
2017-12-16 14:55:12 -0700

    Updated/ New Xcode

--- Swifting/ViewController.swift
+++ Swifting/ViewController.swift
@@ -10,9 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theLabel: UILabel!
+    
+    
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        theLabel.text = "Greetings Gibran!"
+        
+    }
+    @IBAction func dontPush(_ sender: Any) {
+        theLabel.text = "You Rebel, Seriously Though, STOP!"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor.yellow
+        
+        theLabel.text = "Who Goes There?"
+
+    
     }
 
     override func didReceiveMemoryWarning() {
